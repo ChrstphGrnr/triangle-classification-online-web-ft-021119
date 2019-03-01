431053f207b7e34a1105cb7290e4663bc953eb0c
Christoph Gruener <christophgruener@me.com>
2019-03-01 19:38:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,11 @@ class Triangle
       raise TriangleError
     elsif @l_1 + @l_2 <= @l_3 || @l_1 + @l_3 <= @l_2 || @l_2 + @l_3 <= @l_1
       raise TriangleError
-    elsif @l_1 == @l_2
+    elsif @l_1 == @l_2 && @l_1 == @l_3 && @l_2 == @l_3 
+      :equilateral 
+    elsif 
+      
+      
 
 
 
