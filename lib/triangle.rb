e4b1cb1f618702ef4fef85304f19cdbb70581455
Christoph Gruener <christophgruener@me.com>
2019-03-01 18:22:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,16 @@
 class Triangle
-  attr_accessor 
+  attr_accessor :length_one, :length_2, :length_3
+
+  lengths = {length_1: value, length_2: value, length_3: value}
+  
+  def initialize(lengths)
+  end
+
+
+
+  class TriangleError < StandardError
+
+  end
 
 
 end
