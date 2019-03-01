802adf517274f5f58861032c9d52870018fd8fae
Christoph Gruener <christophgruener@me.com>
2019-03-01 18:34:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,8 +4,8 @@ class Triangle
 
 
   def initialize(length_1, length_2, length_3)
-    @length_1 = length_1 
-    @length_2 = length_2 
+    @length_1 = length_1
+    @length_2 = length_2
     @length_3 = length_3
   end
 
