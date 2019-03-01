84225d0ff381fc6f0abfca8539af9bd56909fac2
Christoph Gruener <christophgruener@me.com>
2019-03-01 18:58:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
 
     if @l_1 <= 0 || @length_2 <= 0 || @length_3 <= 0
       raise TriangleError
-    elsif @length_1 + @length_2 < @length_3 || @length_1 + @length_3 < @length_2 || @length_2 + @length_2 <
+    elsif @l_1 + @length_2 < @length_3 || @length_1 + @length_3 < @length_2 || @length_2 + @length_2 <
       raise TriangleError
 
 
