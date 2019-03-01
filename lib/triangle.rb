3c4fb25f1d335dc6d05d431b478b64432520f312
Christoph Gruener <christophgruener@me.com>
2019-03-01 18:41:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,13 +3,14 @@ class Triangle
 
 
 
-  def initialize(lengths)
-    lengths.each {|key, value| self.send("#{key}=", value)}
+  def initialize(length_1: value, length_2:value, length_3: value)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
   end
 
   def kind
     if @length_1 < 0 || @length_2 < 0 || @length_3 < 0
-      raise
   end
 
 
