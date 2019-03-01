119aeb2adfbb110c61b352ac2b7fcd1baaa0f874
Christoph Gruener <christophgruener@me.com>
2019-03-01 18:58:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,16 +4,16 @@ class Triangle
 
 
   def initialize(l_1, l_2, l_3)
-    @length_1 = l_1
-    @length_2 = l_2
-    @length_3 = l_3
+    @l_1 = l_1
+    @l_2 = l_2
+    @l_3 = l_3
   end
 
   def kind
 
     if @length_1 <= 0 || @length_2 <= 0 || @length_3 <= 0
       raise TriangleError
-    elsif @length_1 + @length_2 < @length_3
+    elsif @length_1 + @length_2 < @length_3 || @length_1 + @length_3 < @length_2 || @length_2 + @length_2 <
       raise TriangleError
 
 
