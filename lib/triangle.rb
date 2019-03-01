ceeaf9edbc382f2f95a3ab26d316ee0d775af724
Christoph Gruener <christophgruener@me.com>
2019-03-01 18:53:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   def kind
     if @length_1 < 0 || @length_2 < 0 || @length_3 < 0
       raise TriangleError
-    elsif @length_1 + @length_2 < @length_3 
+    elsif @length_1 + @length_2 < @length_3
       raise TriangleError
 
 
