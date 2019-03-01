a9fbda83e38667876f05beb901f812e5202e73dd
Christoph Gruener <christophgruener@me.com>
2019-03-01 19:39:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,13 +20,8 @@ class Triangle
     elsif @l_1 != @l_2 && @l_1 != @l_3 && @l_2 != @l_3
       :scalene
     elsif elsif @l_1 == @l_2 || @l_1 == @l_3 || @l_2 == @l_3
-
-
-
-
-
-
-
+      :isosceles
+    
     end
 
 
